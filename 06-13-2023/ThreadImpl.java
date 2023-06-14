3354df0da622fb8fe76eb6a77985acb225a4ee6a
Karthik275 <karthikeerishetti@gmail.com>
2023-06-14 15:50:19 -0500

    threads

---
+++ 06-13-2023/ThreadImpl.java
@@ -0,0 +1,10 @@
+
+
+public class ThreadImpl extends Thread {
+    @Override
+    public void run() {
+        for(int i = 1; i < 11; i++) {
+            System.out.println("Thread Class - " +i);
+        }
+    }
+}
